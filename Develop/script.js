cc1749e2aad6506abf388a256bc304ab2d5d21af
Jeremiah Swearingen <jswearingen18@gmail.com>
2022-08-29 15:24:30 -0500

    structured layout for events through a fucntion called createCalendar

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,35 @@
 var dayOfWeek = moment().format("dddd, " + "LL");
 $("#currentDay").text(dayOfWeek);
-function calenderAddTime() {
-    $("<div></div>").after(".container");
+var x = 1;
+function createCalendar() {
+    for (var i = 9; i < 18; i++) {
+var timeEvent = $("<div></div>");
+var saveButton = $("<button>💾</button>");
+var time = $("<div></div>");
+
+if (i <= 11) {
+    $(".container").append(timeEvent);
+    $(timeEvent).addClass("textarea");
+    $(timeEvent).append(saveButton);
+    $(saveButton).addClass("saveBtn");
+    $(timeEvent).append(time, i +"AM");
+    $(time).addClass("hour");
+} else if (i == 12) {
+    $(".container").append(timeEvent);
+    $(timeEvent).addClass("textarea");
+    $(timeEvent).append(saveButton);
+    $(saveButton).addClass("saveBtn");
+    $(timeEvent).append(time, i +"PM");
+    $(time).addClass("hour");
+} else {
+    $(".container").append(timeEvent);
+    $(timeEvent).addClass("textarea");
+    $(timeEvent).append(saveButton);
+    $(saveButton).addClass("saveBtn");
+    $(timeEvent).append(time, x +"PM");
+    $(time).addClass("hour");
+    x++;
 }
-calenderAddTime();
+    }
+}
+createCalendar();
