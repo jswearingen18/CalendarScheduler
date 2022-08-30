290cc441f421ceef48743f4faed098b545cdecf5
Jeremiah Swearingen <jswearingen18@gmail.com>
2022-08-29 19:20:23 -0500

    makes function create an empty div box to format layout

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,46 @@
 var dayOfWeek = moment().format("dddd, " + "LL");
 $("#currentDay").text(dayOfWeek);
 var x = 1;
+
 function createCalendar() {
-    for (var i = 9; i < 18; i++) {
+    for (var i = 7; i < 19; i++) {
+var createBox = $("<div></div>");
 var timeEvent = $("<div></div>");
 var saveButton = $("<button>💾</button>");
-var time = $("<div></div>");
+var time = $("<p></p>");
 
 if (i <= 11) {
-    $(".container").append(timeEvent);
-    $(timeEvent).addClass("textarea");
-    $(timeEvent).append(saveButton);
-    $(saveButton).addClass("saveBtn");
-    $(timeEvent).append(time, i +"AM");
+    $(".container").append(createBox);
+    $(createBox).addClass("row");
+    time.text(i +"AM");
+    $(createBox).append(time);
     $(time).addClass("hour");
+    $(createBox).append(timeEvent);
+    $(timeEvent).addClass("textarea");
+    $(createBox).append(saveButton);
+    $(saveButton).addClass("saveBtn");
+    
 } else if (i == 12) {
-    $(".container").append(timeEvent);
-    $(timeEvent).addClass("textarea");
-    $(timeEvent).append(saveButton);
-    $(saveButton).addClass("saveBtn");
-    $(timeEvent).append(time, i +"PM");
+    $("div.text-center").append(createBox);
+    $(createBox).addClass("row");
+    time.text(i +"PM");
+    $(createBox).append(time);
     $(time).addClass("hour");
+    $(createBox).append(timeEvent);
+    $(timeEvent).addClass("textarea");
+    $(createBox).append(saveButton);
+    $(saveButton).addClass("saveBtn");
+   
 } else {
-    $(".container").append(timeEvent);
-    $(timeEvent).addClass("textarea");
-    $(timeEvent).append(saveButton);
-    $(saveButton).addClass("saveBtn");
-    $(timeEvent).append(time, x +"PM");
+    $("div.text-center").append(createBox);
+    $(createBox).addClass("row");
+    time.text(x +"PM");
+    $(createBox).append(time);
     $(time).addClass("hour");
+    $(createBox).append(timeEvent);
+    $(timeEvent).addClass("textarea");
+    $(createBox).append(saveButton);
+    $(saveButton).addClass("saveBtn");
     x++;
 }
     }
