acca66655fb64f973a470de7268140a90ae5cd03
Jeremiah Swearingen <jswearingen18@gmail.com>
2022-08-31 09:43:50 -0500

    added click events on save button to store locally and get

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,6 @@
+$(document).ready(function(){
+
+
 var dayOfWeek = moment().format("dddd, " + "LL");
 $("#currentDay").text(dayOfWeek);
 var x = 1;
@@ -13,29 +16,35 @@ var time = $("<p></p>");
 if (i <= 11) {
     $("div.text-center").append(createBox);
     $(createBox).addClass("row");
+    $(createBox).attr("id", "hour-"+[i]);
     time.text(i +"AM");
     $(createBox).append(time);
     $(time).addClass("hour");
     $(createBox).append(timeEvent);
+    $(timeEvent).addClass("calendarEvent");
     $(createBox).append(saveButton);
     $(saveButton).addClass("saveBtn");
    
 } else if (i == 12) {
     $("div.text-center").append(createBox);
     $(createBox).addClass("row");
+    $(createBox).attr("id", "hour-"+[i]);
     time.text(i +"PM");
     $(createBox).append(time);
     $(time).addClass("hour");
     $(createBox).append(timeEvent);
+    $(timeEvent).addClass("calendarEvent");
     $(createBox).append(saveButton);
     $(saveButton).addClass("saveBtn");
 } else {
     $("div.text-center").append(createBox);
     $(createBox).addClass("row");
+    $(createBox).attr("id", "hour-"+[i]);
     time.text(x +"PM");
     $(createBox).append(time);
     $(time).addClass("hour");
     $(createBox).append(timeEvent);
+    $(timeEvent).addClass("calendarEvent");
     $(createBox).append(saveButton);
     $(saveButton).addClass("saveBtn");
     x++;
@@ -43,7 +52,6 @@ if (i <= 11) {
 }
 function checkTime() {
     var currentTime = moment().format("HH");
-    console.log(currentTime);
     timeEvent.each( function () {
         if (currentTime > i) {
             $(this).addClass("past")
@@ -58,3 +66,22 @@ checkTime();
     }
 }
 createCalendar();
+$(".saveBtn").on("click", function() {
+    var value = $(this).siblings('.calendarEvent').val();
+    var timeEl = $(this).parent().attr('id');
+    localStorage.setItem(timeEl, value);
+    })
+    $('#hour-6 .calendarEvent').val(localStorage.getItem("hour-6"));
+    $('#hour-7 .calendarEvent').val(localStorage.getItem("hour-7"));
+    $('#hour-8 .calendarEvent').val(localStorage.getItem("hour-8"));
+    $('#hour-9 .calendarEvent').val(localStorage.getItem("hour-9"));
+    $('#hour-10 .calendarEvent').val(localStorage.getItem("hour-10"));
+    $('#hour-11 .calendarEvent').val(localStorage.getItem("hour-11"));
+    $('#hour-12 .calendarEvent').val(localStorage.getItem("hour-12"));
+    $('#hour-13 .calendarEvent').val(localStorage.getItem("hour-13"));
+    $('#hour-14 .calendarEvent').val(localStorage.getItem("hour-14"));
+    $('#hour-15 .calendarEvent').val(localStorage.getItem("hour-15"));
+    $('#hour-16 .calendarEvent').val(localStorage.getItem("hour-16"));
+    $('#hour-17 .calendarEvent').val(localStorage.getItem("hour-17"));
+    $('#hour-18 .calendarEvent').val(localStorage.getItem("hour-18"));
+})
